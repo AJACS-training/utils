83a34b6104524546e34562f2db426574c87805fc
tazro inutano ohta <inutano@gmail.com>
2015-06-23 15:44:50 +0900

    create a new directory for storing images

--- images2md.sh
+++ images2md.sh
@@ -13,9 +13,10 @@ dirname=`echo ${target_dir} | awk -F '/' '{ print $NF }'`
 
 # make new directory and copy images to be uploaded to github
 markdown_dir="./${dirname}-markdown"
-mkdir ${markdown_dir}
-cp ${target_dir}/* ${markdown_dir}
+image_dir="${markdown_dir}/images"
+mkdir -p ${image_dir}
+cp ${target_dir}/* ${image_dir}
 
 # create simple markdown file employ images named readme.md
 cd ${markdown_dir}
-ls | awk '{ print "![" $0 "](" $0 ")\n---\n" }' > ./README.md
+ls images | awk '{ print "![images/" $0 "](images/" $0 ")\n---\n" }' > ./README.md
